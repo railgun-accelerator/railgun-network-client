d67cee3ed77a8b3cbe93f4c9595d052613890b33
zh99998 <zh99998@gmail.com>
2015-11-01 18:30:38 +0800

    region ipset for dnsmasq hack

--- start.sh
+++ start.sh
@@ -8,7 +8,10 @@ ipset create -exist ports1 bitmap:port range 10000-32767
 ipset create -exist ports2 bitmap:port range 10000-32767
 ipset create -exist ports3 bitmap:port range 10000-32767
 ipset create -exist block_ip hash:ip
-ipset restore -file ipset
+ipset create -exist region1 hash:net
+ipset create -exist region2 hash:net
+ipset create -exist region3 hash:net
+ipset create -exist region4 hash:net
 
 echo 'iptables...'
 
