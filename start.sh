5fe5b5ea79c3096f8edab4101b9848dbe769570d
zh99998 <zh99998@gmail.com>
2015-11-05 20:43:54 +0800

    fix

--- start.sh
+++ start.sh
@@ -17,6 +17,12 @@ echo 'iptables...'
 
 envsubst < iptables-rules | iptables-restore
 
+# this server is a gateway
+if [ -n "${RAILGUN_TOS}" ]; then
+    iptables -t nat -A POSTROUTING -o ${RAILGUN_INTERFACE} -s 10.0.0.0/8 -j SNAT --to-source ${RAILGUN_PRIVATE_ADDRESS}
+    iptables -t mangle -A PREROUTING -s 10.0.0.0/16 -p tcp -m addrtype ! --dst-type LOCAL -m tos --tos ${RAILGUN_TOS} -j TPROXY --on-port 5000 --on-ip 0.0.0.0 --tproxy-mark 0x3
+fi
+
 echo 'network...'
 
 coffee main.coffee
